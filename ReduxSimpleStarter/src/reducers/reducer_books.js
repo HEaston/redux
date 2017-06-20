5f74c8dedccb44e2c2cc72876122caaa8312a8c0
Easton, Hayley <heaston@netsuite.com>
2017-06-20 16:00:35 +0100

    Section 4, lesson 41 complete

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'Crime and Punishment' },
+    { title: 'A Short History of Tractors in Hungarian' },
+    { title: 'Vernon God Little' }
+  ]
+}

5c266e9e8d65b0bb57f99f62c9cda8624e732c91
Easton, Hayley <heaston@netsuite.com>
2017-06-30 14:36:28 +0100

    Managing App State with Redux, lesson 45 complete

---
+++ ReduxSimpleStarter/src/actions/index.js
@@ -0,0 +1,5 @@
+export function selectBook(book) {
+  console.log('A book has been selected:', book.title);
+}
+
+// we have to made sure the action creator is wired up to redux
--- ReduxSimpleStarter/src/containers/book_list.js
+++ ReduxSimpleStarter/src/containers/book_list.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -10,6 +13,7 @@ export default class BookList extends Component {
   }
 
   render() {
+    console.log(this.props.asdf);
     return (
       <ul className="list-group col-sm-4">
         {this.renderList()}
@@ -19,3 +23,25 @@ export default class BookList extends Component {
 }
 
 // key should be any unique value
+
+function mapStateToProps(state) {
+  // whatever is returned will show up as props
+  // inside of BookList
+  return {
+    books: state.books,
+    asdf: '123'
+  }
+}
+
+// anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selected is called, the results should be passed
+  // to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// export the container
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make is available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
